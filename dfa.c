702a68183b58221cee0026e05f9b9cc537b0c42e
Charlie Perkins <cperkins@dhcp-10-4-9-92.wireless.rochester.edu>
2016-09-16 14:48:15 -0400

    added code

--- dfa.c
+++ dfa.c
@@ -6,8 +6,9 @@
 
 DFA *
 DFA_new(int n) {
-    DFA dfa = (DFA*)malloc(sizeof(DFA));
+    DFA *dfa = (DFA*)malloc(sizeof(DFA));
     dfa->nstates = n;
     dfa->current_state = 0;
-    
+    dfa->states = (DFA_State*)malloc(n*sizeof(DFA_State));  /* not sure about this statement. Trying to dynamically allocate space */
+    return dfa;  
 }
