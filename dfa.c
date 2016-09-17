232d72e648e71f65768a9fc746fa1a39e83878f6
Luba Le Xuan <lubavable@mail.ru>
2016-09-17 17:44:29 -0400

    Added main

--- dfa.c
+++ dfa.c
@@ -18,17 +18,23 @@ int DFA_get_size(DFA *dfa) {
 }
 
 int DFA_get_transition(DFA *dfa, int statenum, char symbol) {
-	return *dfa->(*(states + statenum))->transitions[symbol];  /*don't know how to use pointers*/
+	return dfa->(*(states + statenum))->transitions[symbol];  /*don't know how to use pointers*/
 	/*states[statenum]*/
 }
 
 void DFA_set_transition(DFA *dfa, int src, char symbol, int dst) {
-	*dfa->(*(states + statenum))->transitions[symbol] = dst;
+	dfa->(*(states + statenum))->transitions[symbol] = dst;
 }
 
 void DFA_set_transition_str(DFA *dfa, int src, char *str, int dst) {
-	for(i=0; *(str + i) != '\0'; i++) {
-		*dfa->(*(states + statenum))->transitions[(*(str + i))] = dst;
+	for(int i=0; *(str + i) != '\0'; i++) {
+		dfa->(*(states + statenum))->transitions[(*(str + i))] = dst;
 	}
 }
 
+int main (int argc, char **argv) {
+	DFA *trial  = DFA_new(3);
+	DFA_get_size(trial);
+	DFA_set_transition(trial,0,'a',1);
+	DFA_get_transition(trial, 0, 'a');
+}
