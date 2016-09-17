4edf3a537dbac99753fcb64eb72337fb90ac4666
Charlie Perkins <cperkins@dhcp-10-4-24-55.wireless.rochester.edu>
2016-09-17 16:31:27 -0400

    added files

--- dfa.c
+++ dfa.c
@@ -4,11 +4,23 @@
 #include <stdbool.h>
 
 
-DFA *
-DFA_new(int n) {
+DFA *DFA_new(int n) {
     DFA *dfa = (DFA*)malloc(sizeof(DFA));
     dfa->nstates = n;
     dfa->current_state = 0;
     dfa->states = (DFA_State*)malloc(n*sizeof(DFA_State));  /* not sure about this statement. Trying to dynamically allocate space */
     return dfa;  
 }
+
+int DFA_get_size(DFA *dfa) {
+	return dfa->nstates;
+}
+
+int DFA_get_transition(DFA *dfa, int statenum, char symbol) {
+	return *dfa->(*(states + statenum))->transitions[symbol];  /*don't know how to use pointers*/
+}
+
+void DFA_set_transition(DFA *dfa, int src, char symbol, int dst) {
+
+}
+
