045f1248259fc6a591621796e1152e0b0d8cf4f2
Charlie Perkins <cperkins@dhcp-10-4-1-117.wireless.rochester.edu>
2016-09-18 13:16:20 -0400

    added functions
    added functions except execute

--- nfa.c
+++ nfa.c
@@ -23,3 +23,44 @@ int NFA_get_size(NFA *nfa) {
 IntSet *NFA_get_transitions(NFA *nfa, int statenum, char symbol) {
 	return nfa->states[statenum]->transitions[symbol];
 }
+
+void NFA_add_transition(NFA *nfa, int src, char symbol, int dst) {
+    IntSet_add(nfa->states[src]->transitions[symbol], dst);
+}
+
+void NFA_add_transition_str(NFA *nfa, int src, char *str, int dst) {
+    for(int i=0; str[i] != '\0'; i++) {
+        IntSet_add(nfa->states[src]->transitions[str[i]], dst);
+    }
+}
+
+void NFA_add_transition_all(NFA *nfa, int src, int dst) {
+    for(int i=0; i<=NFA_NSYMBOLS; i++) {
+        IntSet_add(nfa->states[src]->transitions[i], dst);
+    }
+}
+
+IntSet *NFA_get_current_states(NFA *nfa) {
+    return nfa->current_states;
+}
+
+void NFA_set_current_states(NFA *nfa, IntSet *states) {
+    nfa->current_states = states;
+}
+
+void NFA_set_current_state(NFA *nfa, int state) {
+    IntSet *current = IntSet_New();
+    IntSet_add(current, state);
+    nfa->current_states = current;
+}
+
+bool NFA_get_accepting(NFA *nfa, int statenum) {
+    return nfa->states[statenum].is_accepting;
+}
+
+void NFA_set_accepting(NFA *nfa, int statenum, bool value) {
+    nfa->states[statenum].is_accepting = value; /*will this modify the is_accepting? do i need to point to the value or is this just fine?*/
+}
+
+
+
