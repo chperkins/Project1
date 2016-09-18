e5fbf34cf16a45f5f141305cee84fbb5fb995f0d
Charlie Perkins <cperkins@dhcp-10-5-17-39.wireless.rochester.edu>
2016-09-18 16:55:23 -0400

    fixed some errors

--- nfa.c
+++ nfa.c
@@ -3,6 +3,7 @@
 
 #include <stdio.h> 
 #include <stdbool.h>
+#include <stdlib.h>
 #include "nfa.h"
 #include "IntSet.h"
 
@@ -14,7 +15,7 @@ NFA *NFA_new(int n) {
     nfa->current_states = start;
     nfa->states = (NFA_State*)malloc(n*sizeof(NFA_State));
     for(int i=0; i<n; i++) {
-        nfa->states[i].is_accepting = FALSE;
+        nfa->states[i].is_accepting = 0;
     } /*initializes all accepting values as false*/
     return nfa;
 }
@@ -24,22 +25,22 @@ int NFA_get_size(NFA *nfa) {
 }
 
 IntSet *NFA_get_transitions(NFA *nfa, int statenum, char symbol) {
-	return nfa->states[statenum]->transitions[symbol];
+	return nfa->states[statenum].transitions[symbol];
 }
 
 void NFA_add_transition(NFA *nfa, int src, char symbol, int dst) {
-    IntSet_add(nfa->states[src]->transitions[symbol], dst);
+    IntSet_add(nfa->states[src].transitions[symbol], dst);
 }
 
 void NFA_add_transition_str(NFA *nfa, int src, char *str, int dst) {
     for(int i=0; str[i] != '\0'; i++) {
-        IntSet_add(nfa->states[src]->transitions[str[i]], dst);
+        IntSet_add(nfa->states[src].transitions[str[i]], dst);
     }
 }
 
 void NFA_add_transition_all(NFA *nfa, int src, int dst) {
     for(int i=0; i<=NFA_NSYMBOLS; i++) {
-        IntSet_add(nfa->states[src]->transitions[i], dst);
+        IntSet_add(nfa->states[src].transitions[i], dst);
     }
 }
 
@@ -65,33 +66,8 @@ void NFA_set_accepting(NFA *nfa, int statenum, bool value) {
     nfa->states[statenum].is_accepting = value; /*will this modify the is_accepting? do i need to point to the value or is this just fine?*/
 }
 
-bool NFA_execute(NFA *nfa, char *input) {
-    for(int i = 0; input[i] != '\0'; i++) {
-        /*set destination to be the set off all states we go to with input[i] from current states*/
-        IntSet *destination = IntSet_New();
-        /*we use current_s_iterator to iterate through each state in the current states*/
-        current_s_iterator = IntSet_iterator(nfa->current_states);
-        while(IntSetIterator_has_next(current_s_iterator)) { /*this checks if theres another current state*/
-            tempState = current_s_iterator->node->value; /*temp state is the current state to look at*/
-            IntSet_union(destination, nfa->states[tempState]->transitions[input[i]]); /*this adds all the transitions form tempstate give input to destination*/
-            IntSetIterator_next(current_s_iterator); /*this iterates the while loop*/
-        }
-        nfa->current_states = destination; /*this sets the current state to the set of destination states*/
-    } /*we run this over and over again for each input symbol*/
-
-    /*now we have to find out whether we will accept or not*/
-
-    final_s_iterator = IntSet_iterator(nfa->current_states); /*create another iterator for the set of last states*/
-    while(IntSetIterator_has_next(final_s_iterator)) {
-        tempState = final_s_iterator->node->value;
-        /*if the current state is accepting, return true*/
-        if(nfa->states[tempState].is_accepting == TRUE) {
-            return TRUE
-        }
-    }
-    /*if nothing accepted, return false*/
-    return FALSE;
-
+int main (int argc, char **argv) {
 }
 
 
+#endif
