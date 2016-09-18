ff3263c72e91a4364b0a9916dad5222046ef78c9
Charlie Perkins <cperkins@dhcp-10-4-1-117.wireless.rochester.edu>
2016-09-18 12:02:03 -0400

    added functions to nfa.c

--- nfa.c
+++ nfa.c
@@ -4,11 +4,22 @@
 #include <stdio.h> \
 #include <stdbool.h>
 #include "nfa.h"
+#include "IntSet.h"
 
 NFA *NFA_new(int n) {
-    DFA *dfa = (DFA*)malloc(sizeof(DFA));
-    dfa->nstates = n;
-    dfa->current_state = 0;
-    dfa->states = (DFA_State*)malloc(n*sizeof(DFA_State));  /* not sure about this statement. Trying to dynamically allocate space */
-    return dfa;  
+    NFA *nfa = (NFA*)malloc(sizeof(NFA));
+    nfa->nstates = n;
+    IntSet *start = IntSet_New();
+    IntSet_add(start, 0);
+    nfa->current_states = start;
+    nfa->states = (NFA_State*)malloc(n*sizeof(NFA_State));  /* not sure about this statement. Trying to dynamically allocate space */
+    return nfa;
+}
+
+int NFA_get_size(NFA *nfa) {
+	return nfa->nstates;
+}
+
+IntSet *NFA_get_transitions(NFA *nfa, int statenum, char symbol) {
+	return nfa->states[statenum]->transitions[symbol];
 }
